c2c66cd755b37dfbf878e3ebf0df1022e69ba762
Chun Fung Kwok <jkwok@svi.edu.au>
2022-10-23 12:20:02 +0000

    version 1.1.19

--- inst/plumber/server.R
+++
@@ -1,8 +0,0 @@
-library(plumber)
-
-# 'app.R' is the location of the file containing your endpoint functions
-r <- plumb("app.R")
-
-# get port number from environment variable
-port <- strtoi(Sys.getenv("PORT"))
-r$run(port = port, host = "0.0.0.0")
